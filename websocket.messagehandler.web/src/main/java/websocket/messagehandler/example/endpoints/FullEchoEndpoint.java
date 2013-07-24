fba64353e0e7ef7b5cca19c9c13ec84297f8dfd2
Niki Dokovski <nickytd@gmail.com>
2013-07-24 23:56:47 +0300

    added client example

---
+++ websocket.messagehandler.web/src/main/java/websocket/messagehandler/example/endpoints/FullEchoEndpoint.java
@@ -0,0 +1,39 @@
+package websocket.messagehandler.example.endpoints;
+
+import java.nio.ByteBuffer;
+import java.util.logging.Logger;
+
+import javax.websocket.EndpointConfig;
+import javax.websocket.MessageHandler;
+import javax.websocket.Session;
+
+public class FullEchoEndpoint extends javax.websocket.Endpoint {
+
+	private final static Logger log = Logger.getLogger(FullEchoEndpoint.class.getSimpleName());
+	
+	@Override
+	public void onOpen(Session session, EndpointConfig config) {
+		final String sessionId = session.getId();
+		log.info("established session with id: "+sessionId);
+		
+		//add text based message handler
+		session.addMessageHandler(new MessageHandler.Whole<String>() {
+			
+			@Override
+			public void onMessage(String msg) {
+				log.info(sessionId+": text message: "+msg);
+			}
+		});
+		
+		//add binary based message handler
+		session.addMessageHandler(new MessageHandler.Whole<ByteBuffer>() {
+
+			@Override
+			public void onMessage(ByteBuffer buffer) {
+				log.info(sessionId+": binary message: "+new String(buffer.array()));
+			}
+		});
+		
+	}
+
+}
