a7ba820f79a7a97bc3402058d507271c60e3fdf1
Niki Dokovski <nickytd@gmail.com>
2013-07-25 10:24:47 +0300

    add ping message handling

--- websocket.messagehandler.client/src/main/java/websocket/messagehandler/example/App.java
+++ websocket.messagehandler.client/src/main/java/websocket/messagehandler/example/App.java
@@ -1,7 +1,5 @@
 package websocket.messagehandler.example;
 
-
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -21,26 +19,37 @@ import javax.websocket.WebSocketContainer;
 import websocket.messagehandler.example.endpoint.Endpoint;
 
 public class App {
-	
-	
+
 	static CountDownLatch latch = new CountDownLatch(1);
-	public static void main( String[] args ) throws DeploymentException, IOException, URISyntaxException, InterruptedException {
-    	
-		WebSocketContainer container  = ContainerProvider.getWebSocketContainer();
-		Session session = container.connectToServer(Endpoint.class, new URI("ws://localhost:8080/websocket-messages/partialEchoEndpoint"));
+
+	public static void main(String[] args) throws DeploymentException,
+			IOException, URISyntaxException, InterruptedException {
+
+		WebSocketContainer container = ContainerProvider
+				.getWebSocketContainer();
+		Session session = container.connectToServer(Endpoint.class, new URI(
+				"ws://localhost:8080/websocket-messages/partialEchoEndpoint"));
 		ByteBuffer buffer = ByteBuffer.allocate(1024);
 		buffer.put(new String("Here is a message!").getBytes("UTF-8"));
 		buffer.flip();
 		session.getAsyncRemote().sendBinary(buffer, new SendHandler() {
-			
+
 			@Override
 			public void onResult(SendResult result) {
-				System.out.println("is send result ok: "+result.isOK());
+				System.out.println("is send result ok: " + result.isOK());
 				latch.countDown();
 			}
 		});
-		
-		latch.await(10,TimeUnit.SECONDS);
-    	session.close(new CloseReason(CloseCodes.NORMAL_CLOSURE,"sesison close"));
-    }
+
+		latch.await(10, TimeUnit.SECONDS);
+		session.close(new CloseReason(CloseCodes.NORMAL_CLOSURE,
+				"sesison close"));
+
+		session = container.connectToServer(Endpoint.class, new URI(
+				"ws://localhost:8080/websocket-messages/fullEchoEndpoint"));
+
+		Thread.sleep(1000);
+		session.close(new CloseReason(CloseCodes.NORMAL_CLOSURE,
+				"sesison close"));
+	}
 }
