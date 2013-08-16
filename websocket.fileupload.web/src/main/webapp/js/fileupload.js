c4d5b40a0b6ba7ea89b0fcc3a45d0215c05d1d74
Niki Dokovski <nickytd@gmail.com>
2013-08-16 09:53:25 +0300

    clean up javascript

--- websocket.fileupload.web/src/main/webapp/js/fileupload.js
+++ websocket.fileupload.web/src/main/webapp/js/fileupload.js
@@ -1,5 +1,4 @@
-var BUFF_SIZE = 512;
-var buffer = new Array(BUFF_SIZE);
+var BUFF_SIZE = 1024;
 var socket = 0;
 
 function check() {
@@ -25,10 +24,38 @@ function check() {
 		}
 
 		socket = new WebSocket(ws_url);
-		socket.onopen = wsOpen;
-		socket.onerror = wsError;
-		socket.onmessage = wsMessage;
-		socket.onclose = wsClose;
+		socket.onopen = function (){
+			console.log('ws connection is established');
+			var msg = new Object();
+			msg.op = 'list';
+			console.log('send text: ' + JSON.stringify(msg));
+			socket.send(JSON.stringify(msg));
+		};
+		
+		socket.onerror = function(error){
+			console.log('ws error: '+error)
+		};
+		
+		socket.onmessage = function(evt){
+			console.log('received msg: ' + evt.data);
+			var files = JSON.parse(evt.data);
+			var table = document.getElementById('list');
+			while (table.rows.length > 0)
+				table.deleteRow(table.rows.length - 1);
+
+			for ( var i = 0, f; f = files[i]; i++) {
+				var row = table.insertRow(i);
+				var cell1 = row.insertCell(0);
+				var cell2 = row.insertCell(1);
+				cell1.innerHTML = f.name;
+				cell2.innerHTML = f.size;
+			}
+			document.getElementById('list_zone').style.height = 'auto';
+		};
+
+		socket.onclose = function(){
+			console.log('close');
+		};
 
 	} else {
 		alert('This browser does not support websockets');
@@ -53,62 +80,29 @@ function uploadFile(evt) {
 	msg.fname = f.name;
 	msg.fsize = f.size;
 	socket.send(JSON.stringify(msg));
+	console.log('send test: '+JSON.stringify(msg));
 	
 	for ( var j = 0; j < chunks; j++) {
 		var reader = new FileReader();
 
-		reader.onload = sendBinary;
+		reader.onload = function(evt) {
+			var data = evt.target.result.slice(0);
+			console.log('send binary: ' + data.byteLength);
+			socket.binaryType = "arraybuffer";
+			socket.send(data);
+		};
+		
 		var upper = BUFF_SIZE * (j + 1);
 		if (upper > f.size) {
 			upper = f.size;
-			window.setTimeout(listFiles, 2000);
+			window.setTimeout(function () {
+				var msg = new Object();
+				msg.op = 'list';
+				console.log('send text: ' + JSON.stringify(msg));
+				socket.send(JSON.stringify(msg));
+			}, 2000);
 		}
 		var blob = f.slice(BUFF_SIZE * j, upper);
 		reader.readAsArrayBuffer(blob);
 	}
-}
-
-function sendBinary(evt) {
-	var data = evt.target.result.slice(0);
-	console.log('send binary: ' + data.byteLength);
-	socket.binaryType = "arraybuffer";
-	socket.send(data);
-	console.log(socket.bufferedAmount);
-}
-
-function listFiles() {
-	var msg = new Object();
-	msg.op = 'list';
-	console.log('send text: ' + JSON.stringify(msg));
-	socket.send(JSON.stringify(msg));
-}
-
-function wsOpen() {
-	console.log('ws connection is established');
-	listFiles();
-}
-
-function wsError(error) {
-	conosle.error(error);
-}
-
-function wsMessage(evt) {
-	console.log('received msg: ' + evt.data);
-	var files = JSON.parse(evt.data);
-	var table = document.getElementById('list');
-	while (table.rows.length > 0)
-		table.deleteRow(table.rows.length - 1);
-
-	for ( var i = 0, f; f = files[i]; i++) {
-		var row = table.insertRow(i);
-		var cell1 = row.insertCell(0);
-		var cell2 = row.insertCell(1);
-		cell1.innerHTML = f.name;
-		cell2.innerHTML = f.size;
-	}
-	document.getElementById('list_zone').style.height = 'auto';
-}
-
-function wsClose(evt) {
-	console.log('close: ' + evt);
 }
